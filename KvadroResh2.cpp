6ed794f9d0b93861cd474c0c4548d327d3cb60cd
ArFar1975 <142601704+ArFar1975@users.noreply.github.com>
2023-08-18 19:37:53 +0300

    сравнения стали безопаснее

--- KvadroResh2.cpp
+++ KvadroResh2.cpp
@@ -15,42 +15,44 @@ int main()
 
 int solver()
 {
-    float a, b, c, d, x1, x2;
+    const double ERR = 0.1e5;
+    double a, b, c, d, x1, x2;
 
     printf("a*x^2 + b*x + c = 0\n\n");
     printf("Введите a:");
-    scanf("%f", &a);
-    if (a == 0) {
-        printf("\aОшибка! При нулевом коэффиценте уравнение перестаёт быть квадратным!\n");
-        printf("Попробуйте ввести \"a\" снова!\n");
-        solver();
-    }
-    else {
+    scanf("%lf", &a);
+    if (fabs(a) > ERR) {
         printf("Введите b:");
-        scanf("%f", &b);
+        scanf("%lf", &b);
         printf("Введите c:");
-        scanf("%f", &c);
+        scanf("%lf", &c);
         printf("\n");
 
         d = b * b - 4 * a * c;
 
-        if (d < 0) {
+        if (a < -ERR) {
+            system("cls");
             printf("\aОшибка! Уравнение не имеет решения в вещественных числах.\n");
             printf("Попробуйте снова!\n");
             solver();
         }
-        else if (d == 0) {
-            x1 = -b / (2 * a);
-            if (x1 == 0)
-                printf("x = 0");
-            else
-                printf("x = %.2f", x1);
-        }
-        else {
+        else if (a > ERR) {
             x1 = (-b + sqrt(d)) / (2 * a);
             x2 = (-b - sqrt(d)) / (2 * a);
-            printf("x1 = %.2f,\nx2 = %.2f", x1, x2);
+            printf("x1 = %.4f,\nx2 = %.4f", x1, x2);
         }
+        else {
+            x1 = -b / (2 * a);
+            if (fabs(x1) > ERR)
+                printf("x = %.4f", x1);
+            else
+                printf("x = 0");
+        }
+    }
+    else {
+        printf("\aОшибка! При нулевом коэффиценте уравнение перестаёт быть квадратным!\n");
+        printf("Попробуйте ввести \"a\" снова!\n");
+        solver();
     }
 
     return 0;
