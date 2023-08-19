24094636d9b800a494e26f3aaabe4a8b1149ab7e
ArFar1975 <142601704+ArFar1975@users.noreply.github.com>
2023-08-19 23:14:52 +0300

    Летели качели
    1

--- KvadroResh5.cpp
+++ KvadroResh5.cpp
@@ -5,6 +5,7 @@ int main()
 {
     double a, b, c, d;
     printf("Hello! This program can solve ...\na*x^2 + b*x + c = 0\n");
+
     while (fabs(a-0) < ESP) {
         printf("Put \"a\" here:");
         scanf("%lf", &a);
@@ -15,14 +16,15 @@ int main()
     scanf("%lf", &b);
     printf("Put \"c\" here:");
     scanf("%lf", &c);
+
     d = b*b - 4.0*a*c;
     if (d < 0) {
         printf("This equation has no real roots");
-        }
+    }
     else if (d > 0) {
         printf("x1 = %g\n", (-b + sqrt(d)) / (2.0*a));
         printf("x2 = %g\n", (-b - sqrt(d)) / (2.0*a));
-        }
+    }
     else {
         printf("x = %g\n", (-b/(2.0*a)));
     }
